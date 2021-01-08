7de15c2eeeb837ebd0516d5590c9367acc56a818
jibachhydv <jibachhydv18@gmail.com>
2021-01-08 08:41:53 +0545

    Updates

---
+++ precache-manifest.02f28174e69ea83a88e363156492667d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69758141969c5f0b6e1c7237d7acb870",
+    "url": "/developerfolio/index.html"
+  },
+  {
+    "revision": "0442b77ba9dbb506ead7",
+    "url": "/developerfolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "cf6a97e43f6f2e0c3d1a",
+    "url": "/developerfolio/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "e854731ec9dde9177510",
+    "url": "/developerfolio/static/css/main.8d2bfe42.chunk.css"
+  },
+  {
+    "revision": "babea4574e1f63dc8277",
+    "url": "/developerfolio/static/js/2.8dce1465.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/developerfolio/static/js/2.8dce1465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0442b77ba9dbb506ead7",
+    "url": "/developerfolio/static/js/3.1986bd9d.chunk.js"
+  },
+  {
+    "revision": "cf6a97e43f6f2e0c3d1a",
+    "url": "/developerfolio/static/js/4.ee79a3d3.chunk.js"
+  },
+  {
+    "revision": "e854731ec9dde9177510",
+    "url": "/developerfolio/static/js/main.465e6dbd.chunk.js"
+  },
+  {
+    "revision": "0a871634530ea13cbf1b",
+    "url": "/developerfolio/static/js/runtime-main.67520fa7.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "d38a06be295aab3c83c2a9611eb17c6c",
+    "url": "/developerfolio/static/media/hackerrankLogo.d38a06be.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "fc4a0e43ec8e1353e318ce33d0157bec",
+    "url": "/developerfolio/static/media/manakamanaLogo.fc4a0e43.jpeg"
+  },
+  {
+    "revision": "08d43085861360355d368e410656b50b",
+    "url": "/developerfolio/static/media/onestepnepalLogo.08d43085.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "95b8871451be5e53aaa6e0b84792ba74",
+    "url": "/developerfolio/static/media/sxcLogo.95b88714.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
